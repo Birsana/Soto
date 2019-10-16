f85fe22f831b40cec578f2a47a72d4dd02d7c98e
Birsana <andrebirsa2@gmail.com>
2019-10-15 22:01:04 -0400

    version 3

---
+++ GalleryShare/Helpers/SendCell.swift
@@ -0,0 +1,24 @@
+//
+//  SendCell.swift
+//  GalleryShare
+//
+//  Created by Andre Birsan on 2019-10-12.
+//  Copyright © 2019 Andre Birsan. All rights reserved.
+//
+
+import UIKit
+
+class SendCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
