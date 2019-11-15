0d8372f5f49aeaccd5215a64e326739ea2e98889
Birsana <andrebirsa2@gmail.com>
2019-11-15 15:24:57 -0500

    CV 2/2

---
+++ GalleryShare/Container View Controllers/HomeGalPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeGalPageViewController.swift
+//  
+//
+//  Created by Andre Birsan on 2019-11-14.
+//
+
+import UIKit
+
+class HomeGalPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+}
